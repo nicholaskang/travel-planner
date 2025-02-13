9fe252a019be684072f728966e39ae95408c33a4
Nick Kang <nickkang257@gmail.com>
2025-02-12 22:48:34 -0800

    Comment, to test git config

--- src/components/MultiStepForm.tsx
+++ src/components/MultiStepForm.tsx
@@ -21,7 +21,7 @@ export default function MultiStepForm(): JSX.Element {
     handleNextStep();
   };
 
-  // Generating Gemini Response
+  // Fetch Gemini Response
   const apiKey = import.meta.env.VITE_GEMINI_API_KEY;
   const genAI = new GoogleGenerativeAI(apiKey);
   const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
